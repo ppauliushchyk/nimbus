6bfadee565aca09d1e9d3989e0021666e60ec93f
Pavel Pauliushchyk <ppauliushchyk@gmail.com>
2024-12-07 22:09:15 +0200

    feat: 👽 added not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function NodFound() {
+  return (
+    <div className="container">
+      <div className="row pt-5">
+        <div className="col-12 col-lg-6 mx-auto">
+          <div className="text-center">
+            <h1 className="display-5 fw-bold text-body-emphasis">Not Found</h1>
+
+            <p className="lead">
+              Looks like you took a wrong turn at Albuquerque. 👽
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
